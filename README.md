e2668f73b9c7bc9b18b3102ba0dc9bfccd650735
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2019-04-01 16:50:10 +0700

    create bash file python for switch version 2 to version 3

---
+++ README.md
@@ -0,0 +1,3 @@
+How to run bash file for mac
+1) open terminal
+2) type "sh filename.sh"
---
+++ python/swich-python-3.sh
@@ -0,0 +1,9 @@
+# switch python version 3
+echo "Switch python version 3"
+alias python=python3
+echo "\n"
+echo "python version: "
+python --version
+echo "\n"
+echo "python path"
+which python
